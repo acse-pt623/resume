6c2a85dba68399ce4cd7b012f68edd294ac9f03c
ptan <peifeng.tan23@imperial.ac.uk>
2024-05-30 00:03:35 +0100

    finish

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-docker build -t sb2nov/latex .
-docker run --rm -i -v "$PWD":/data sb2nov/latex pdflatex sourabh_bajaj_resume.tex
+# latexmk -pdf Peifeng_Tan_resume.tex
+latexmk -pdf 谭沛烽_简历.tex
+
